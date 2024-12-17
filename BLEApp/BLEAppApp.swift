e9665fe5b9fea01236690ffa6a15b2825856268e
SabalKatuwal <sabal.katuwal1@gmail.com>
2024-12-17 11:54:13 +0545

    Initial Commit

---
+++ BLEApp/BLEAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BLEAppApp.swift
+//  BLEApp
+//
+//  Created by Sabal on 12/17/24.
+//
+
+import SwiftUI
+
+@main
+struct BLEAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
